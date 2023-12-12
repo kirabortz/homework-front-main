7790568e1e84d7ec0467568eb140204394cddea7
BladeDancer <69kir969@mail.ru>
2023-12-12 19:09:46 +0300

    Edit all PropsType. Create variable (filteredUseres) and add filter func

--- src/HW/2/HW2.tsx
+++ src/HW/2/HW2.tsx
@@ -10,11 +10,11 @@ export type UserType = {
   id: number;
   name: string;
   age: number;
-  address: any; // пропиши типизацию
+  address: AddressType
 };
 
 export type UsersObjectType = {
-  myFriends: any; // пропиши типизацию
+  myFriends: UserType[]
 };
 
 export const HW2 = () => {
@@ -45,12 +45,10 @@ export const HW2 = () => {
   }
 
   let [currentUsers, setCurrentUsers] = useState<UsersObjectType>(users);
-
-  const filterUsers = () => {
-    const filteredUsers = 'НУЖНО ПРОФИЛЬТРОВАТЬ ДРУЗЕЙ. ОСТАВЛЯЕМ ТОЛЬКО ТЕХ, КОТОРЫЕ ЖИВУТ В ГОРОДЕ LOS ANGELES';
+  const filterUsers = (city:string) => {
+    const filteredUsers = currentUsers.myFriends.filter((user)=> user.address.city === city)
     setCurrentUsers({ myFriends: filteredUsers });
   };
-
   return (
     <div id={'hw02'}>
       <UserList2 users={currentUsers} filterUsers={filterUsers} />
